8a499f5e47d37fc4e6f1ac81b0f55319a7644d15
Colin Tseng <klt.aus@yahoo.com>
2021-07-12 14:31:28 +1000

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        self.word = word
+    end
+
+    def match(array)
+
+        
+        array.filter do | item |
+
+            splitWord = item.split("")
+            splitTitle = word.split("")
+            
+            splitTitle.sort == splitWord.sort 
+               
+        end
+
+    
+    end
+
+end
